b93dc7fae2288035342b6bc13c537d216aa52b47
baerelektro <alex@134-0-115-155.ovz.vps.regruhosting.ru>
2021-05-03 19:59:38 +0000

    Fixes bug xy

---
+++ hello_freetonia/target/debug/deps/hello_freetonia-52909893e802dc31.d
@@ -0,0 +1,5 @@
+/home/alex/freetonia/hello_freetonia/target/debug/deps/hello_freetonia-52909893e802dc31: src/main.rs
+
+/home/alex/freetonia/hello_freetonia/target/debug/deps/hello_freetonia-52909893e802dc31.d: src/main.rs
+
+src/main.rs:
---
+++ hello_freetonia/target/debug/hello_freetonia.d
@@ -0,0 +1,1 @@
+/home/alex/freetonia/hello_freetonia/target/debug/hello_freetonia: /home/alex/freetonia/hello_freetonia/src/main.rs
